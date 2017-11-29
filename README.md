3cfd7dc8a7c676271ae4883337f68da86e53d89d
Marco Gomez <me@mgz.me>
2017-11-28 23:48:27 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Improved Netstat
+
+This is a netstat + whois that I wrote to replace the Linux's netstat as it better fits my needs. Besides showing all the current IPv4 connections, it shows some extra information that can't be obtained using Linux's netstat.
+
+![](http://mgz.me/wp-content/uploads/2017/11/inetstat_working.gif)
+
+* Both netstat and whois features are self implemented through reading the proc filesystem and sockets (respectively), so the code does not need to execute none of those Linux command line tools;
+* Local IP address is also obtained by establishing a socket connection with a fast response DNS server and getting it's own connection name, to prevent undesired local IP address as 127.0.0.1;
+* My whois implementation checks "whois.cymru.com" through sockets, passing the requested remote hosts's IPs as a query parameter and requesting a verbose information, thus, the queries return [Autonomous System][1] information as BGP prefixes and AS names, so I can identify to which company that remote host belongs;
+* All the AS information is written / cached to disk through _pickle (Python 3.5 implementation of cPickle), so new queries looking for information previously obtained does not spend time unnecessarily;
+* The queries to check for AS information are executed through multiprocessing to save time once they're required, and all the necessary IPs to be checked are evenly divided through separate lists taking in consideration an optimize number of simultaneous threads to be used.
+
+**#TODO**:
+1. Implement the methods required to merge information concerning IPv6 and open UDP connections as well
+2. Document it properly and implement tests to make sure unexpected behavior break some stuff, considering I plan to use this code to further applications (as important as some fancy Desktop conky showoff :P
+3. Detect 256 colors terminal emulation capabilities (using another project I'm working on - available on GitHub: [color_ansi_rgb][2]) to make colorized output available (as per future Python argparse implementation) to improve readability
+4. Improve some methods that by now feel _a little bit hackish_ to me  
+
+[1]: https://en.wikipedia.org/wiki/Autonomous_system_(Internet)
+[2]: https://github.com/mgzme/color_ansi_rgb
---
+++ inetstat.py
@@ -0,0 +1,372 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import pwd
+import glob
+import re
+import socket
+import time
+import struct
+from collections import OrderedDict
+from multiprocessing import Process, Manager, cpu_count
+try:
+    import _pickle as pickle  # Python 3.5 import of cPickle
+except ImportError:
+    import pickle
+
+python_exec = sys.executable
+python_version = sys.version
+
+current_path = os.getcwd()
+script_path = os.path.dirname(os.path.realpath(__file__))
+python_path = os.path.dirname(python_exec)
+
+running_arch = "{0} bits".format(8 * struct.calcsize("P"))
+threads_available = int(cpu_count() / 2) + 1
+
+system_pid_pattern = '/proc/[0-9]*/fd/[0-9]*'
+sc_clk_tck = os.sysconf_names['SC_CLK_TCK']
+clock_tick = os.sysconf(sc_clk_tck)
+
+kernel_tcp4_info = '/proc/net/tcp'
+tcp_timers = {'00': 'no_timer', '01': 'retransmit', '02': 'keep_alive', '03': 'time_wait', '04': 'window_probe'}
+
+tcp_states = {'01': 'ESTABLISHED', '02': 'SYN_SENT', '03': 'SYN_RECV', '04': 'FIN_WAIT1', '05': 'FIN_WAIT2',
+              '06': 'TIME_WAIT', '07': 'CLOSE', '08': 'CLOSE_WAIT', '09': 'LAST_ACK', '0A': 'LISTEN', '0B': 'CLOSING'}
+
+
+def split_lists(original_list, max_slices):
+    """ Split a list into a list of small lists given the desired number of sub lists """
+    slices = max_slices - 1
+    original_list_size = len(original_list)
+    split_index = int(original_list_size / slices)
+    return [original_list[x:x + split_index] for x in range(0, len(original_list), split_index)]
+
+
+def hex2dec(hex_value):
+    """ Returns a decimal representation of a given hex value"""
+    return str(int(hex_value, 16))
+
+
+def reversed_endian2octets(hex_ip):
+    """ IPs on /proc/net/tcp are stored as big-endian value interpreted as per the machine "endianness", which means it
+    ends up reversed on little-endian machines """
+    reversed_bytes = [hex_ip[6:8], hex_ip[4:6], hex_ip[2:4], hex_ip[0:2]]
+    octets = [hex2dec(_oct) for _oct in reversed_bytes]
+    return '.'.join(octets)
+
+
+def get_pid_of_inode(inode):
+    """ Check which running PID is using the given inode """
+    for inode_pid in glob.glob(system_pid_pattern):
+        try:
+            if re.search(inode, os.readlink(inode_pid)):
+                return inode_pid.split('/')[2]
+        except FileNotFoundError:
+            return '-NA-'
+
+
+def timers_and_jiffies(tcp_timer, jiffy):
+    """ Use Kernel constant values for clock in Hz and estimate jiffies based time from /proc/net/tcp. It returns the
+     timer_type, the timer countdown itself, and the int(max_length) of the timer_type (useful for pretty printing) """
+    tcp_timer_type = tcp_timers[tcp_timer]
+    _time = int(int(hex2dec(jiffy)) / clock_tick)  # int int to round secs (human-readable value)
+    tcp_timer = "{0}s".format(_time) if _time > 0 else '--'
+    return tcp_timer_type, tcp_timer
+
+
+class MinimalWhois(object):
+
+    def __init__(self):
+        """ This is my minimalistic whois implementation using sockets. It's inherited by INetStat class, and it's
+        purpose is to return ASN related information against a given IP address """
+
+        self.host = "whois.cymru.com"
+        self.whois_port = 43
+        self.ipcheck_port = 80
+        self.ipcheck_address = "8.8.8.8"
+
+        self.timeout = 2
+        self.object_flags = " -v {0}\r\n"
+
+        self.socket = None
+        self.sock_family, self.sock_type = socket.AF_INET, socket.SOCK_STREAM
+        self.sock_type_2 = socket.SOCK_DGRAM
+
+        self.local_ip = self.check_local_ip()
+
+    def check_local_ip(self):
+        """ As long as I'm already using sockets, let's use a socket connection to 8.8.8.8 to get our local IP address
+        as any other method will return 127.0.0.1 as per all other Linux methods characteristics """
+        self.socket = socket.socket(self.sock_family, self.sock_type_2)
+        try:
+            self.socket.connect((self.ipcheck_address, self.ipcheck_port))
+            return self.socket.getsockname()[0]
+        except socket.error as socket_error:
+            print('Socket Error:', socket_error)
+            sys.exit(1)
+
+    def lookup(self, ip_address):
+        """ Performs socket connection with "whois.cymru.com" passing the given IP as flag and returns response """
+        self.socket = socket.socket(self.sock_family, self.sock_type)
+        _response = b''
+        try:
+            self.socket.settimeout(self.timeout)
+            self.socket.connect((self.host, self.whois_port))
+            self.socket.send(bytes(self.object_flags.format(ip_address).encode()))
+
+            while True:
+                _data = self.socket.recv(4096)
+                _response += _data
+                if not _data:
+                    break
+            self.socket.close()
+        except socket.error:
+            return None
+
+        _response = _response.decode('utf-8', 'replace')
+        return _response
+
+    def parse_data(self, dictionary, ip_address):
+        """ Receives a multiprocessing managed dictionary and an IP address to perform a lookup method and parse
+         all the returned information concerning the IP's ASN information. Retries 3 times in case of a timeout """
+        _retries = 3
+        _whois_data = self.lookup(ip_address)
+        while _whois_data is None and _retries > 0:
+            print("Retrying ip {0} after it's time out in 3 seconds!".format(ip_address))
+            _retries -= 1
+            _whois_data = self.lookup(ip_address)
+
+        if len(_whois_data) and isinstance(_whois_data, str):
+            _lines = [_line for _line in _whois_data.splitlines()[:2]]
+            _keys, _values = [[_item.lstrip().rstrip() for _item in _line.split('|')] for _line in _lines]
+            _keys = [_key.lower().replace(' ', '_') for _key in _keys]
+            _values = [_value.split(',')[0] for _value in _values]
+            dictionary.update({ip_address: dict(zip(_keys, _values))})
+
+
+class MinimalNetstat(MinimalWhois):
+
+    def __init__(self):
+        """ This is my Python 3 netstat implementation. My intention here is not reinvent the wheel. Instead of the
+        default Linux netstat's behaviour, my implementation will describe and monitor states and timers. We're
+         inheriting my whois implementation to have proper access to my local ip """
+
+        super(MinimalNetstat, self).__init__()
+
+        self.tcp4 = kernel_tcp4_info
+        self.states = tcp_states
+        self.tcp4_data = self.parse_tcp4_data()
+        if self.tcp4_data is not None:
+            self.netstat = self.tcp4_data
+        else:
+            print("Could not retrieve TCP data.")
+            sys.exit(1)
+
+    def read_proc_tcp4(self):
+        """ Reads the data on /proc/net/tcp to get all currently available IPv4 TCP connections """
+        try:
+            with open(self.tcp4, 'r') as _proc:
+                return [_line.replace('\n', '') for _line in _proc.readlines()[1:] if len(_line)]
+        except IOError:
+            return None
+
+    def parse_tcp4_data(self):
+        """ Get information about all currently available IPv4 TCP connections using the read_proc_tcp4 method and
+        parse the information through some conversion methods as per Linux Kernel conventions """
+        _tcp4_data = dict()
+        _data = self.read_proc_tcp4()
+        if _data is None:
+            return _data
+
+        for _entry in _data:
+            _cells = _entry.split()
+
+            _id = _cells[0].replace(':', '')
+
+            _hex_local_host, _hex_local_port = _cells[1].split(':')
+            _local_host, _local_port = reversed_endian2octets(_hex_local_host), hex2dec(_hex_local_port)
+
+            _hex_remote_host, _hex_remote_port = _cells[2].split(':')
+            _remote_host, _remote_port = reversed_endian2octets(_hex_remote_host), hex2dec(_hex_remote_port)
+
+            if _remote_host != '0.0.0.0':
+
+                _layer = 'secure' if _remote_port == '443' else 'insecure'
+                _state = self.states[_cells[3]]
+
+                _timer, _jiffy = _cells[5].split(':')
+                _timer_type, _timer = timers_and_jiffies(_timer, _jiffy)
+
+                _uid = pwd.getpwuid(int(_cells[7]))[0]
+
+                _inode = _cells[9]
+                _inode_pid = get_pid_of_inode(_inode)
+
+                try:
+                    _app = os.readlink("/proc/{0}/exe".format(_inode_pid))  # .split(os.path.sep)[-1]
+                except FileNotFoundError:
+                    _app = '--NA--'
+
+                _app = _app.split(os.path.sep)[-1] if len(_app) > 30 else _app
+
+                _tcp4_entry = {'id': _id, 'state': _state, 'localhost': _local_host, 'lport': _local_port,
+                               'remotehost': _remote_host, 'rport': _remote_port, 'time': _timer, 'timer': _timer_type,
+                               'user': _uid, 'inode': _inode, 'pid': _inode_pid, 'app': _app, 'layer': _layer,
+                               'ipv': 'IPv4'}
+
+                _tcp4_data.update({_remote_host: _tcp4_entry})
+
+        return _tcp4_data
+
+
+class PickleDict:
+
+    def __init__(self):
+        """ Handles storage dictionaries to dist through cpickle and reading them to act as a cache on disk for ASN
+         information concerning IP addresses already queried. TODO: store timestamp to query again if the stored record
+         is older than X days (to check if a block of IPs now belongs to a different company """
+        self.pickle_file = "asn_info.pickle"
+        self.pickle_path = script_path
+        self.my_pickle = os.path.join(self.pickle_path, self.pickle_file)
+
+    def touch(self):
+        """ This method is only being used while I write this code, to "reset" our cache for testing purposes """
+        try:
+            open(self.my_pickle, 'w').close()
+        except IOError:
+            print("Can't touch {0} file!".format(self.my_pickle))
+            sys.exit(1)
+
+    def read(self):
+        """ Read the cache file from disk """
+        if os.path.isfile(self.my_pickle):
+            try:
+                with open(self.my_pickle, 'rb') as _pickle:
+                    return pickle.load(_pickle)
+            except IOError:
+                print("Can't read {0} file (inaccessible or corrupt)!".format(self.my_pickle))
+                sys.exit(1)
+        else:
+            return False
+
+    def write(self, pickle_data):
+        """ Writes the given dictionary (pickle_data) to disk """
+        try:
+            with open(self.my_pickle, 'wb') as _pickle:
+                pickle.dump(pickle_data, _pickle)
+        except IOError:
+            print("Can't write {0} file!".format(self.my_pickle))
+            sys.exit(1)
+
+
+class INetstat(MinimalNetstat):
+
+    def __init__(self):
+        """ We're inheriting MinimalNetstat Class (which inherits MinimalWhois) and we'll also store a timestamp right
+         at the initialization so we can further check the execution time (for testing purposes) """
+        self.start = time.time()
+        super(INetstat, self).__init__()
+
+        self.pickle_dict = PickleDict()
+        self.open_connections = len(self.netstat)
+
+        self.asn_data = self.get_asn_data()
+
+        for mutual_key, values in self.netstat.items():
+            self.netstat[mutual_key] = {**self.netstat[mutual_key], **self.asn_data[mutual_key]}
+
+        def sort_items(dictionary):
+            return dictionary[1]['as_name'], dictionary[1]['timer']
+
+        _netstat_sorted_items = sorted(self.netstat.items(), key=sort_items)
+        self.ordered_netstat = OrderedDict(_netstat_sorted_items)
+
+    def read_asn_data_from_disk(self):
+        """ Return our ASN cache from disk (cpickle stored file) or None if we could not find a cache file """
+        _asn_data = self.pickle_dict.read()
+        return _asn_data if _asn_data else None
+
+    def get_asn_data(self):
+        """ This method identifies which IP addresses are unknown to our cache file and prepare them to be queried.
+         The list of IPs to query are divided into smaller lists concerning the number of available threads on the
+         system, as we're multiprocessing the method that perform que queries to optimize execution time"""
+        _ips_list = [_ip_address for _ip_address in self.netstat.keys()]
+
+        _unknown_asn_data = list()
+        _known_asn_data = self.read_asn_data_from_disk()
+
+        if _known_asn_data and isinstance(_known_asn_data, dict):
+            _unknown_asn_data = [_ip_address for _ip_address in _ips_list if _ip_address not in _known_asn_data]
+        else:
+            _known_asn_data = dict()
+            _unknown_asn_data = [_ip_address for _ip_address in _ips_list if _ip_address not in _unknown_asn_data]
+
+        manager = Manager()
+        asn_dictionary = manager.dict()
+
+        if len(_unknown_asn_data) > threads_available:
+            _chunks_to_process = split_lists(_unknown_asn_data, threads_available)  # divides the list into smaller ones
+
+            for _chunk in _chunks_to_process:  # start the query processes in chunks (concerning lists to be queried)
+                job = [Process(target=self.parse_data, args=(asn_dictionary, _ip)) for _ip in _chunk]
+                _ = [p.start() for p in job]
+                _ = [p.join() for p in job]
+        else:
+            job = [Process(target=self.parse_data, args=(asn_dictionary, _ip)) for _ip in _unknown_asn_data]
+            _ = [p.start() for p in job]
+            _ = [p.join() for p in job]
+
+        _complete_asn_data = {**asn_dictionary, **_known_asn_data}  # merge the previous known data (cache) with new one
+        self.pickle_dict.write(_complete_asn_data)
+        return _complete_asn_data
+
+
+def dict_values_len(dictionary):
+    """ Reads the given dictionary and return a new one containing each one of it's keys with it's correspondent length,
+    which represents whe length of the largest value attributed to that same key"""
+    _values_len_dict = dict()
+    for k, v in dictionary.items():
+        for _k, _v in v.items():
+            if _k not in _values_len_dict or _values_len_dict[_k] < len(_v):
+                _length = len(_v) if len(_v) >= 5 else 5
+                _values_len_dict.update({_k: _length})
+    return _values_len_dict
+
+
+def pretty_print(string, string_type=None):
+    """ Take care of determining which fields should be justified to each side to improve readability """
+    _string_length = pprint_dict[string_type] if string_type is not None else pprint_dict[string]
+    _right_justified_strings = ['localhost', 'remotehost', 'timer', 'bgp_prefix']
+    if string_type is not None and string_type in _right_justified_strings:
+        return string.rjust(_string_length)
+    elif not string_type and string in _right_justified_strings:
+        return string.rjust(_string_length)
+    else:
+        return string.ljust(_string_length)
+
+
+def print_inetstat():
+    """ Print inetstat results to the terminal """
+    keys_to_print = ['localhost', 'lport', 'state', 'remotehost', 'rport', 'layer', 'ipv', 'pid', 'app', 'timer',
+                     'time', 'cc', 'allocated', 'bgp_prefix', 'as_name']
+
+    for key in keys_to_print:
+        print(pretty_print(key), end=' ')
+    sys.stdout.write('\n')
+
+    for key, value in inetstat_dict.items():
+        for _key in keys_to_print:
+            print(pretty_print(value[_key], _key), end=' ')
+        sys.stdout.write('\n')
+
+
+if __name__ == "__main__":
+    inetstat = INetstat()
+    inetstat_dict = inetstat.ordered_netstat
+    pprint_dict = dict_values_len(inetstat_dict)
+    print_inetstat()
+    end = time.time()
+    print("exec time: {0:.2f}s".format(end - inetstat.start))
